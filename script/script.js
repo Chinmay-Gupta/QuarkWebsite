cbbacf9a0a50442768126692c9596bd253239acc
Chinmay-Gupta <chinmaygupta3@gmail.com>
2018-11-22 16:50:49 +0530

    predesigned heading

---
+++ script/script.js
@@ -0,0 +1,23 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	rotateSidenavCog();
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+	rotateSidenavCogReverse();
+} 
+
+function rotateSidenavCog()
+{
+	document.getElementById("sidenavCog").style.transform="rotate(360deg)";
+}
+
+function rotateSidenavCogReverse()
+{
+	document.getElementById("sidenavCog").style.transform="rotate(0deg)";
+}
